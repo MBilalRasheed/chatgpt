678e571a7d4035cbf47dc00d1cc665ce3f2925ee
raza imran khan <razaimrankhan@gmail.com>
2023-05-09 00:08:37 +0500

    worked on responsive header

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -3,47 +3,88 @@ import "./footer.css"
 import { Box, Container, Grid, Stack, } from "@mui/material"
 import ChromeLogo from "../../images/ChromeLogo.png"
 import footResourceImage from '../../images/footResourceImage.png'
+import mainlogo from '../../images/mainlogo.jpg'
 const Footer = () => {
   return (
-    <div style={{width:'auto',height:'auto'}}>
-     <div className="image-class">
+    <div style={{ width: 'auto', height: 'auto' }}>
+      <div className="image-class">
         <img src={footResourceImage} />
       </div>
       <div className="footer-container" >
-      <div className="colorparent" direction={'column'} width={'50%'}>
-        <p className="leftHeadings">Install AIPRM</p>
-        <p className="leftHeadings2">Why waste more time?</p>
-        <p className="leftHeadings">Take back some of those hours
-          and get started with AIPRM today.</p>
-        <div className="subcribe-containter">
-          <div className="chromeExtentionBox1" >
-            <img src={ChromeLogo} alt="extention" />
-            <p style={{marginTop:'auto',marginBottom:'auto'}}>Install for Free</p>
+        <div className="colorparent" direction={'column'} width={'50%'}>
+          <p className="leftHeadings">Install AIPRM</p>
+          <p className="leftHeadings2">Why waste more time?</p>
+          <p className="leftHeadings">Take back some of those hours
+            and get started with AIPRM today.</p>
+          <div className="subcribe-containter">
+            <div className="chromeExtentionBox1" >
+              <img src={ChromeLogo} alt="extention" />
+              <p style={{ marginTop: 'auto', marginBottom: 'auto' }}>Install for Free</p>
+            </div>
+            <div>
+              <p className="leftHeadings3">Already have the extension?</p>
+              <p className="leftHeadings4">Subscribe to a Premium Plan.</p>
+            </div>
           </div>
-          <div>
-          <p className="leftHeadings3">Already have the extension?</p>
-          <p className="leftHeadings4">Subscribe to a Premium Plan.</p>
+
         </div>
-        </div>
-        
-      </div>
         <div className="row-class">
-          <div className="fetures-flex">
-            <p> Features</p>
-            <p> Use Case #1</p>
-            <p> Use Case #2</p>
-            <p> Use Case #3</p>
-            <p> Use Case #4</p>
-            <p> Use Case #4</p>
+          <div className="bizbio">
+            <div style={{ marginTop: '30px' }}>
+              <img src={mainlogo} />
+            </div>
+            <p >
+              This will be the<br />
+              Biz Bio Tagline right here.
+            </p>
+          </div>
+
+          <div className="headerName">
+            <div className="fetures-flex">
+              <p> Features</p>
+              <p> Use Case #1</p>
+              <p> Use Case #2</p>
+              <p> Use Case #3</p>
+              <p> Use Case #4</p>
+              <p> Use Case #4</p>
+            </div>
+            <div className="fetures-flex">
+              <p> Pricings</p>
+              <p> FAQs</p>
+              <p> Referral Program</p>
+              <p> Testimonials</p>
+            </div>
+            <div className="fetures-flex">
+              <p> Prompts</p>
+              <p> CATEGORY #1</p>
+              <p> CATEGORY #2</p>
+              <p> CATEGORY #3</p>
+            </div>
+            <div className="fetures-flex">
+              <p> About</p>
+              <p> Company</p>
+              <p> About Us</p>
+              <p> Contact Us</p>
+
+            </div>
+            <div className="fetures-flex">
+              <p> Resources</p>
+              <p> Blog</p>
+              <p> Webinars</p>
+            </div>
+            <div className="fetures-flex">
+              <p> Community</p>
+              <p> Docs</p>
+              <p> Zendesk</p>
+              <p> Status</p>
+              <p> fin</p>
+            </div>
           </div>
         </div>
-     
+
+      </div>
     </div>
 
-   
-    
-    </div>
-    
   )
 }
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,6 +5,37 @@ import "./layout.css"
 // Header and Footer
 import Header from "./Header/header"
 import Footer from "./Footer/footer"
+import { styled } from '@mui/material/styles';
+import { Stack } from "@mui/material";
+import { red, green, blue } from '@mui/material/colors';
+
+const Root = styled(Stack)(({ theme }) => {
+  
+  // return {
+  //     position: 'relative', bottom: '500px' 
+  // }
+  return {
+    padding: theme.spacing(1),
+    [theme.breakpoints.down('md')]: {
+      position: 'relative',
+      bottom: '400px',
+      zIndex:-1
+    },
+    [theme.breakpoints.up('md')]: {
+      position: 'relative', 
+      bottom: '400px', 
+      zIndex:-1
+    },
+    [theme.breakpoints.up('lg')]: {
+      // backgroundColor: green[500],
+      height:"200px",
+      position: 'absolute', 
+      bottom: '1000px', 
+      zIndex:2,
+      
+    },
+  }
+});
 
 const Layout = ({ children }) => {
   return (
@@ -12,10 +43,17 @@ const Layout = ({ children }) => {
       <div>
         <Header />
         <main style={{
-          background: "#f8f8f8", clipPath: 'polygon(0 0, 100% 0%, 100% 99%, 0 100%)',margin:'0px',position:'relative'}}>{children}</main>
-        <div style={{position:'relative',bottom:'73px'}}>
-        <Footer />
+          background: "#f8f8f8",
+          // clipPath: 'polygon(0 0, 100% 0%, 100% 99%, 0 100%)',
+          margin: '0px',
+        }}>
+          {children}</main>
+        <div className="relative">
+          <div className="absolute">
+            <Footer />
+          </div>
         </div>
+       
       </div>
     </>
   )
